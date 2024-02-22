0e60a9d4bcf8f620754ef94db1aa3afb3840e894
munishmangla98 <munishmangla98@gmail.com>
2024-02-22 11:51:34 +0530

    Book Review

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { BookComponent } from './book/book.component';
+import { BookReadComponent } from './book-read/book-read.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { AdminComponent } from './admin/admin.component';
+import { AddBookComponent } from './add-book/add-book.component';
+import { UpdateBookComponent } from './update-book/update-book.component';
+import { BookUpdateComponent } from './book-update/book-update.component';
+import { BookAddComponent } from './book-add/book-add.component';
+import { BookRetriveComponent } from './book-retrive/book-retrive.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    BookComponent,
+    BookReadComponent,
+    ContactUsComponent,
+    HeaderComponent,
+    LoginComponent,
+    SignupComponent,
+    AdminComponent,
+    AddBookComponent,
+    UpdateBookComponent,
+    BookUpdateComponent,
+    BookAddComponent,
+    BookRetriveComponent
   ],
   imports: [
     BrowserModule,
